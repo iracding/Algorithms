e97728aac92565e9fa11346c9cabf9ee38dc314c
ding.dongsheng <39110620+dongshengfengniaowu@users.noreply.github.com>
2019-08-20 13:32:41 +0800

    Initial commit

---
+++ UnitTest/AlgorithmsTests/BubbleSorterTest.cs
@@ -0,0 +1,19 @@
+﻿using Algorithms.Sorting;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace UnitTest.AlgorithmsTests
+{
+    public static class BubbleSorterTest
+    {
+        [Fact]
+        public static void DoTest()
+        {
+            var list = new List<long>() { 23, 42, 4, 16, 8, 15, 3, 9, 55, 0, 34, 12, 2, 46, 25 };
+            list.BubbleSort();
+            long[] sortedList = { 0, 2, 3, 4, 8, 9, 12, 15, 16, 23, 25, 34, 42, 46, 55 };
+            Assert.True(list.SequenceEqual(sortedList));
+        }
+    }
+}
---
+++ UnitTest/BucketSorterTest.cs
@@ -0,0 +1,19 @@
+﻿using Algorithms.Sorting;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+namespace UnitTest.AlgorithmsTests
+{
+    public static class BucketSorterTest
+    {
+        [Fact]
+        public static void DoTest()
+        {
+            IList<int> list = new List<int>() { 23, 42, 4, 16, 8, 15, 3, 9, 55, 0, 34, 12, 2, 46, 25 };
+            list.BucketSort();
+            int[] sortedList = { 0, 2, 3, 4, 8, 9, 12, 15, 16, 23, 25, 34, 42, 46, 55 };
+            Assert.True(list.SequenceEqual(sortedList));
+        }
+    }
+}
+
